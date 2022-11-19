70b983fb283bf08de07fea109eeb1252e5f48677
sapeol <sudeep.kuchara@protonmail.com>
2022-11-19 20:56:43 +0530

    added vue store

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,26 @@ if (document.querySelector('#collection-page-container')) {
         delimiters: ['${', '}'],
         data() {
             return {
-                test: 'test'
+                data: {
+                    id: 44040886911277,
+                    quantity: 1
+                }
+            }
+        },
+        created() {
+            axios.get()
+        }
+        ,
+        methods: {
+            addtobag(event) {
+                event.preventDefault()
+                let data = this.data
+                console.log(data)
+                axios.post('/cart/add.js', data)
+                    .then((response) => {
+                        console.log(response)
+                    })
+                    .catch(errors => console.log(errors))
             }
         }
 
